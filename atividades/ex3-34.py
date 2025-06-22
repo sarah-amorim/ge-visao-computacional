e4187da7e0fabccbc875e41aff70613ebc80f2b9
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-22 18:27:01 -0300

    Update

---
+++ atividades/ex3-34.py
@@ -0,0 +1,14 @@
+'''
+In the original image used to generate the three blurred images shown,
+the vertical bars are 5 pixels wide, 100 pixels high, and their separation
+is 20 pixels. The image was blurred using square box kernels of size
+23, 25 and 45 elements on the side, respectively. The vertical bars on the 
+left, lower part of (a) and (c) are blurred, but a clear separation exists between 
+them. 
+'''
+
+'''
+However, the bars have merged in image (b), despite the fact that the kernel 
+used to generate this image is much smaller than the kernel that 
+produced image (c). Explain the reason for thia.
+'''
