58ecc7d35f6fe873153b3101deef71a717f6b7c8
Sarah Amorim <162258709+sarah-amorim@users.noreply.github.com>
2025-06-22 11:01:50 -0300

    Update

--- atividades/ex3-26.py
+++ atividades/ex3-26.py
@@ -1,1 +1,8 @@
+'''
+The two images shown in the following figure are quite different, but their histograms are the same.
+Suppose that each image is blurred using a 3 x 3 box kernel. 
+'''
 
+# (a) Would the histograms of the blurred images still be equal? Explain.
+
+# (b) If your answer is no, either sketch the two histograms or give two tables detailing the histogram components. 
